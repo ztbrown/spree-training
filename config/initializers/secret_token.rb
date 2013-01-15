54e3cb82ce73c810e1a0377c9930fbfb10ea3432
Zack Brown <zbrown@ubuntu.(none)>
2013-01-14 17:25:06 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeaconStreet::Application.config.secret_token = '531dfd7286b41f2b8c88750115fa26548c1991a56bc42d4f7dadfcc35d63c8bb1ef4cd34c2a45a5d1e2069d9e15f6cc27909bba65fef10065153425f45e9b152'
